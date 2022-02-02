8997d3a48a0a99430f9a3215b02e9e1ea74bc4be
EunsilChoi92 <atheung1@naver.com>
2022-02-02 19:42:21 +0900

    창 띄우기

--- tikinter_1_Window.py
+++ tikinter_1_Window.py
@@ -1,1 +1,8 @@
-print("하")
+from tkinter import *
+
+win = Tk()  # 창 생성
+win.geometry("500x500") # 창 크기
+win.title("제목") # 창 제목
+win.option_add("*Font", "맑은고딕 25")  # 폰트, 폰트 크기(지정 안 하면 글자 크기가 작음)
+
+win.mainloop()  # 창 실행
